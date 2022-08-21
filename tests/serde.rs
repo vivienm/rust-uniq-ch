968a30193532ef0114797fed01128d65c9faae12
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2022-08-21 13:55:16 +0200

    test: add serialization test

---
+++ tests/serde.rs
@@ -0,0 +1,33 @@
+#![cfg(feature = "serde")]
+
+use std::assert_eq;
+
+use highway::HighwayBuildHasher;
+use uniq_ch::Bjkst;
+
+const SERIALIZED: &'static str = "[\
+4,4,0,false,[\
+null,6120665149970903695,null,null,null,null,null,793563473396344097,\
+null,null,null,16707926894801950140,null,null,14520555826795478172,null\
+]]";
+
+#[test]
+fn serialize() {
+    let bjkst = Bjkst::<u32, HighwayBuildHasher>::from_iter([1, 2, 3, 4]);
+    let serialized = serde_json::to_string(&bjkst).unwrap();
+    assert_eq!(serialized, SERIALIZED);
+}
+
+#[test]
+fn deserialize() {
+    let mut bjkst: Bjkst<u32, HighwayBuildHasher> = serde_json::from_str(SERIALIZED).unwrap();
+    assert_eq!(bjkst.len(), 4);
+
+    // Insert an element already present in the BJKST. The count should not change.
+    bjkst.insert(&4);
+    assert_eq!(bjkst.len(), 4);
+
+    // Insert a new element. The count should change.
+    bjkst.insert(&5);
+    assert_eq!(bjkst.len(), 5);
+}
